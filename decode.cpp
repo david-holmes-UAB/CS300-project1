0ee5f6d3bd8a9e1d8920a8b35a683b0678051881
Ruler (David) <azurecode516@gmail.com>
2023-09-11 19:51:26 -0500

    Updated initial files

--- decode.cpp
+++ decode.cpp
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <string>
+#include <iostream>
 
+#define KEYBOARD std::string "QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./"
+
+std::string decypher(std::string &input);
 
 int main() {
+
+    std::string usrInput;
+    std::cout << "Enter user input: \n";
+
+    getline(std::cin, usrInput);
+    //std::cout << usrInput << " was typed.";
+
+    //std::cout << "Output: " << decypher(usrInput) << "\n";
+
+
     
+}
+
+std::string decypher(std::string &input) {
+    std::string output = "";
+    for (char curr : input) {
+        // TODO
+    }
+
+
+
+    return output;
 }
