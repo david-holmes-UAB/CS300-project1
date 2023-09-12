5239e6562c71bbffb2db9cb92db2e184957c7fd3
Ruler (David) <azurecode516@gmail.com>
2023-09-11 20:57:19 -0500

    Added user input functionality

--- revAdd.cpp
+++ revAdd.cpp
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
 
+#define MAX_ITER 1000
+#define MAX_PALIN 4294967295
+
+int reverseNum(int &fwd);
+void rev_and_add(int &test);
 
 int main() {
+
+    int cases;
+    std::cout << "Enter the number of test cases, press enter, then enter cases line by line:\n";
+    std::cin >> cases;
+
+    std::vector<int> testers;
+
+    int n;
+    for(size_t i = 0; i < cases; i++) {
+        std::cin >> n;
+        testers.push_back(n);
+    }
+    
+
     
 }
